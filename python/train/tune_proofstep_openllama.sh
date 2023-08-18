da4d0e53d5359997df1aa04c689a9f912fbdef93
zhangir-azerbayev <zazerbayev@gmail.com>
2023-08-18 08:47:28 -0600

    incredibly janky finetuning code

--- python/train/tune_proofstep_openllama.sh
+++ python/train/tune_proofstep_openllama.sh
@@ -34,8 +34,8 @@ export TOKENIZERS_PARALLELISM=false
 BASE_DIR=$(pwd)
 TRAIN_FILE=${BASE_DIR}/data/leandojo_benchmark_4/processed/proofstep-train.jsonl
 VALID_FILE=${BASE_DIR}/data/leandojo_benchmark_4/processed/proofstep-val.jsonl
-MODEL=openlm-research/open_llama_3b
-CONFIG=${BASE_DIR}/deepspeed_config.json
+MODEL=openlm-research/open_llama_3b_v2
+CONFIG=${BASE_DIR}/deepspeed_config_alt.json
 
 OUTDIR=./model/${MODEL}
 
@@ -46,19 +46,19 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7  ${BASE_DIR}/tune.py \
     --valid_data_path ${VALID_FILE} \
     --fp16 \
     --output_dir ${OUTDIR} \
-    --max_steps 500 \
+    --max_steps 20000 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 2 \
     --evaluation_strategy "steps" \
     --eval_steps 500 \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 20000 \
     --save_total_limit 1 \
     --learning_rate 3e-5 \
     --load_best_model_at_end 1 \
     --weight_decay 0. \
-    --warmup_ratio 0.05 \
+    --warmup_ratio 0.04 \
     --lr_scheduler_type "linear" \
     --logging_steps 10 \
     --logging_dir "$OUTDIR" \
